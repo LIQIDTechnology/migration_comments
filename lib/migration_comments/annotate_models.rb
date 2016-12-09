3f728704bb97074574f616c25429a746b12bae00
marius <marius.liwotto@gmail.com>
2016-12-09 13:51:52 +0100

    changed column regex & add annotate comments column

--- lib/migration_comments/annotate_models.rb
+++ lib/migration_comments/annotate_models.rb
@@ -21,14 +21,24 @@ module MigrationComments
         column_comments = adapter.retrieve_column_comments(table_name)
         lines = []
         info.each_line{|l| lines << l.chomp}
-        column_regex = /^#\s+(\w+)\s+:\w+/
+        column_regex = /^#\s\*\*([`\w|*]+)\s+/
         len = lines.select{|l| l =~ column_regex}.map{|l| l.length}.max
         lines.each do |line|
           if line =~ /# Table name: |# table \+\w+\+ /
             line << " # #{table_comment}" if table_comment
           elsif line =~ column_regex
-            comment = column_comments[$1.to_sym]
-            line << " " * (len - line.length) << " # #{comment}" if comment
+            column_name = $1.gsub(/[\*,`]/, '')
+            comment = column_comments[column_name.to_sym]
+            line << " " * (len - line.length)
+            line << "|"
+            line << " #{comment}" if comment
+          elsif line.starts_with?('# Name')
+            line << " " * (len - line.length)
+            line << "| Comments"
+          elsif line.starts_with?('# ------')
+            line << "-" * [len - line.length, 0].max
+            line << "|"
+            line << "-" * 25
           end
         end
         lines.join($/) + $/
